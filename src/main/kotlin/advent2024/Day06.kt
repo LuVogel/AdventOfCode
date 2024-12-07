f5a298507252453c741059f8de6db0827f6efadb
LuVogel <vogel.lukas@hotmail.com>
2024-12-07 15:10:27 +0100

    Day06 Part1

--- src/main/kotlin/advent2024/Day06.kt
+++ src/main/kotlin/advent2024/Day06.kt
@@ -3,12 +3,134 @@ package advent2024
 import utils.BasePuzzle
 
 class Day06 : BasePuzzle() {
+
+    val guards = listOf(">", "<", "^", "v")
+    var guardPos = Pair(0, 0)
+    var direction = ""
+    var initialDirection = ""
+    var initialPosition = Pair(0, 0)
+
+
     override fun puzzle1(filePath: String): Any {
         val input = getInput(filePath)
-        return 0
+        val matrixList = mutableListOf<MutableList<String>>()
+        for (line in input) {
+            matrixList.add(line.toList().map { it.toString() }.toMutableList())
+        }
+        val guardPosList = matrixList.flatMapIndexed { rowIndex, row ->
+            row.mapIndexedNotNull { colIndex, element ->
+                if (element in guards) Pair(colIndex, rowIndex) else null
+            }
+        }
+        assert(guardPosList.size == 1)
+        guardPos = guardPosList.first()
+        direction = getDirection(matrixList)
+        return walkAndMark(matrixList)
+    }
+
+    private fun getDirection(matrixList: MutableList<MutableList<String>>): String {
+        val s = matrixList[guardPos.second][guardPos.first]
+        if (s.equals(">")) return "right"
+        else if (s.equals("<")) return "left"
+        else if (s.equals("^")) return "up"
+        else if (s.equals("v")) return "down"
+        else return "end"
+    }
+
+    val visitedPos = mutableSetOf<Pair<Int, Int>>()
+
+    private fun walkAndMark(matrixList: MutableList<MutableList<String>>): Any {
+        visitedPos.add(guardPos)
+        while (guardPos.first in 0 until matrixList.size &&
+            guardPos.second in 0 until matrixList[0].size) {
+            visitedPos.add(moveIntoDirection(matrixList))
+        }
+        return visitedPos.toSet().size - 1
+    }
+
+
+    private fun moveIntoDirection(matrixList: MutableList<MutableList<String>>): Pair<Int, Int> {
+        if (direction == "left") {
+            if (guardPos.first-1 >= 0 ) {
+                if (matrixList[guardPos.second][guardPos.first-1] == "#") {
+                    direction = "up"
+                    return moveIntoDirection(matrixList)
+                }
+            }
+            guardPos = Pair(guardPos.first-1, guardPos.second)
+
+        } else if (direction == "right") {
+            if (guardPos.first+1 < matrixList[0].size) {
+                if (matrixList[guardPos.second][guardPos.first+1] == "#") {
+                    direction = "down"
+                    return moveIntoDirection(matrixList)
+                }
+            }
+            guardPos = Pair(guardPos.first+1, guardPos.second)
+
+        } else if (direction == "up") {
+            if (guardPos.second-1 >= 0) {
+                if (matrixList[guardPos.second - 1][guardPos.first] == "#") {
+                    direction = "right"
+                    return moveIntoDirection(matrixList)
+                }
+            }
+            guardPos = Pair(guardPos.first, guardPos.second-1)
+
+
+        } else if (direction == "down") {
+            if (guardPos.second+1 < matrixList.size) {
+                if (matrixList[guardPos.second+1][guardPos.first] == "#") {
+                    direction = "left"
+                    return moveIntoDirection(matrixList)
+                }
+            }
+            guardPos = Pair(guardPos.first, guardPos.second+1)
+        }
+        return guardPos
     }
 
     override fun puzzle2(filePath: String): Any {
-        TODO("Not yet implemented")
+        val input = getInput(filePath)
+        val matrixList = mutableListOf<MutableList<String>>()
+        for (line in input) {
+            matrixList.add(line.toList().map { it.toString() }.toMutableList())
+        }
+        val guardPosList = matrixList.flatMapIndexed { rowIndex, row ->
+            row.mapIndexedNotNull { colIndex, element ->
+                if (element in guards) Pair(colIndex, rowIndex) else null
+            }
+        }
+        assert(guardPosList.size == 1)
+        guardPos = guardPosList.first()
+        initialPosition = guardPosList.first()
+        initialDirection = getDirection(matrixList)
+        direction = getDirection(matrixList)
+        var loops = 0
+        for (i in matrixList.indices) {
+            for (j in matrixList[i].indices) {
+                if (matrixList[i][j] == ".") {
+                    matrixList[i][j] = "#"
+                    println("replace $i, $j")
+                    loops += walkAndSearchForLoop(matrixList)
+                    matrixList[i][j] = "."
+                    guardPos = initialPosition
+                    direction = initialDirection
+                }
+            }
+        }
+        return loops
+
+    }
+
+    private fun walkAndSearchForLoop(matrixList: MutableList<MutableList<String>>): Int {
+        while (guardPos.first in 0 until matrixList.size &&
+            guardPos.second in 0 until matrixList[0].size) {
+            moveIntoDirection(matrixList)
+            if (direction == initialDirection && guardPos == initialPosition) {
+                return 1
+            }
+        }
+        return 0
     }
 }
--- src/test/kotlin/advent2024/Day06Test.kt
+++ src/test/kotlin/advent2024/Day06Test.kt
@@ -1,4 +1,36 @@
 package advent2024
 
-class Day06Test {
+import utils.TestPuzzle
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day06Test : TestPuzzle() {
+
+    @Test
+    override fun puzzle1Test() {
+        val day06 = Day06()
+        val res = day06.puzzle1("advent2024/Day06/test-input.txt")
+        assertEquals(41, res)
+    }
+
+    @Test
+    override fun puzzle1Input() {
+        val day06 = Day06()
+        val res = day06.puzzle1("advent2024/Day06/input1.txt")
+        assertEquals(4903, res)
+    }
+
+    @Test
+    override fun puzzle2Test() {
+        val day06 = Day06()
+        val res = day06.puzzle2("advent2024/Day06/test-input.txt")
+        assertEquals(6, res)
+    }
+
+    @Test
+    override fun puzzle2Input() {
+        val day06 = Day06()
+        val res = day06.puzzle2("advent2024/Day06/input1.txt")
+        assertEquals(4903, res)
+    }
 }
